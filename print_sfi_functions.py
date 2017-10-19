ee8d501ce78f3ddc67efad0aa3d2611d2cb328cc
Jonathan <31544952+TheMadWhiteHatter@users.noreply.github.com>
2017-10-19 08:51:11 -0700

    Add files via upload

---
+++ print_sfi_functions.py
@@ -0,0 +1,14 @@
+def main():
+	print('Owen Wilson is kind of a terrible actor!') 
+	print('Wow you are definitley correct about that!')
+	print('Bro Swagger Souls is a dying Youtube channel') 
+main()
+def floatcalc():
+	print('There is no such thing as floating, only buoyancy')
+floatcalc()
+def intcalc():
+	print('You do not need a calculator to define your intelligence')
+intcalc()
+def stringcalc():
+	print('Strings hold life itself together')
+stringcalc() 
